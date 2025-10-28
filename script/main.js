bfec709f0e20fdc586a59e1340b3948987bcdba2
kietworkspaceoffice-cell <kietworkspace.office@gmail.com>
2025-10-28 08:35:01 +0000

    add filter function and fix some layout

--- script/main.js
+++ script/main.js
@@ -2,9 +2,14 @@ fetch('./data/data.json')
 .then(res => res.json())
 .then(data =>{
     const container = document.querySelector('.ext-cards');
+    const allBtn = document.getElementById('btn-all');
+    const activeBtn = document.getElementById('btn-active');
+    const inactiveBtn = document.getElementById('btn-inactive');
 
 
-    data.forEach((ext, index) => {
+    function renderList(list) {
+      container.innerHTML = '';
+      list.forEach((ext, index) => {
         const item = document.createElement('li');
         item.className = 'ext-cart';
         item.innerHTML = `<header class="ext-header">
@@ -23,6 +28,53 @@ fetch('./data/data.json')
           </footer>`;
 
           container.appendChild(item);
+
+        //   Gắn biến sự kiện
+        const removeExt = item.querySelector('.ext-remove');
+        const changeActive = item.querySelector('input[type="checkbox"]');
+
+        // Sự kiện remove
+        removeExt.addEventListener('click', () => {
+            item.remove();
+            
+            const extIndex = data.indexOf(ext);
+            if (extIndex > -1) {
+                data.splice(extIndex,1);
+            };
+            renderList(data);
+        })
+
+        // Sự kiện change active
+        changeActive.addEventListener('change', () => {
+            ext.isActive = changeActive.checked;
+        })
+      })
+    }
+    
+    // render lại toàn bộ
+    renderList(data);
+
+    // các nút filter
+
+    allBtn.addEventListener('click', () => renderList(data));
+    activeBtn.addEventListener('click', () => {
+      const filtered = data.filter(ext => ext.isActive);
+      renderList(filtered);
+    });
+    inactiveBtn.addEventListener('click', () => {
+      const filtered = data.filter(ext => !ext.isActive);
+      renderList(filtered);
     })
-})
+  })
 .catch(error => console.error('Lỗi khi đọc JSON:', error));
+
+
+const filterBtn = document.querySelectorAll('.filter-btn');
+filterBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    // remove tất cả active button khác
+    filterBtn.forEach(b => b.classList.remove('active'));
+    // thêm active cho button hiện tại
+    btn.classList.add('active');
+  });
+});
